c7866ba9302e144b84d5476e27c1fb5b68660dcf
dkalabukhov <daniil.kalabukhov@gmail.com>
2024-09-09 00:02:00 +0300

    Added components to the  homepage, finished tabs

--- 07-lection5/03-toggler/toggler.js
+++ 07-lection5/03-toggler/toggler.js
@@ -1,20 +1,23 @@
-const togglerInput = document.querySelectorAll('.toggler__input');
+(function changeTogglerTitle() {
+  const togglerInput = document.querySelectorAll('.toggler__input');
 
-const render = () => {
-  togglerInput.forEach((toggler) => {
-    if (toggler.checked) {
-      toggler.parentElement.querySelector('.toggler__title').textContent = 'On';
-    } else {
-      toggler.parentElement.querySelector('.toggler__title').textContent = 'Off';
-    }
-    if (toggler.disabled) {
-      toggler.parentElement.querySelector('.toggler__title').textContent = 'Disabled';
-    }
-  })
-}
+  const render = () => {
+    togglerInput.forEach((toggler) => {
+      if (toggler.checked) {
+        toggler.parentElement.querySelector('.toggler__title').textContent = 'On';
+      } else {
+        toggler.parentElement.querySelector('.toggler__title').textContent = 'Off';
+      }
+      if (toggler.disabled) {
+        toggler.parentElement.querySelector('.toggler__title').textContent = 'Disabled';
+      }
+    })
+  }
 
-togglerInput.forEach((input) => {
-  input.addEventListener('change', (e) => {
-    render();
-  })
-});
+  togglerInput.forEach((input) => {
+    input.addEventListener('change', (e) => {
+      render();
+    })
+  });
+})();
+
